1ecf8a40da82ecedfe3fdb70f34362cadd7a6289
HBM1@evergreen.edu <D3UsR2M8UZWc>
2022-01-11 18:07:01 -0500

    switched exec implementation

--- cpu-api/Lab2.c
+++ cpu-api/Lab2.c
@@ -5,6 +5,8 @@
 int
 main(int argc, char *argv[])
 {
-	char *args[]={"ls",NULL};
-	execv(args[0],args);
+	char *myargs[2];
+	myargs[0] = strdup("ls");   // program: "wc" (word count)
+	myargs[1] = NULL;           // marks end of array
+	execvp(myargs[0], myargs);  // runs word count
 }
